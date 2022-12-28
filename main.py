21d106f6ec0d24741c63bfdac20edd92bd0b0bb1
pub-fn <globally.scoped@yahoo.com>
2022-12-28 18:13:36 -0500

    Further progress on DataModel. Creation and saving working. Soon encryption will follow after testing that saving & loading is stable.

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import scilla_db
 
-print(scilla_db.database.create_account(
+new_account = scilla_db.database.create_account(
     username="username",
     password="password"
-))
+)
+
+new_data = scilla_db.database.create_account_data(
+    account=new_account,
+    data={
+
+    }
+)
--- scilla_db/database.py
+++ scilla_db/database.py
@@ -3,6 +3,7 @@ from scilla_db import gate
 import secrets
 import hashlib
 import base64
+import json
 
 database = SqliteDatabase("test.db")
 database.connect()
@@ -27,6 +28,18 @@ class DataModel(__BaseModel):
 
 
 database.create_tables([AccountModel, DataModel])
+
+
+def get_data_by_account(
+    account: AccountModel
+) -> DataModel:
+    __result = None
+    try:
+        __result = DataModel.get(
+            DataModel.account == account
+        )
+    finally:
+        return __result
 
 
 def get_account(
@@ -58,12 +71,54 @@ def create_account(
 
     new_account = AccountModel.create(
         account_username=username,
-        account_password=base64.b64encode(
-            hashlib.sha256(
-                password.encode()
-            ).digest()
-        ),
+        account_password=hashlib.sha256(
+            password.encode()
+        ).hexdigest(),
         account_salt=base64.b64encode(salt)
     )
 
     return new_account
+
+
+def create_account_data(
+    account: AccountModel,
+    data: dict = {}
+) -> DataModel:
+    __account_type = type(account)
+    __account_str = str(account)
+
+    __data_type = type(data)
+    __data_str = str(data)
+
+    if __account_type is not AccountModel:
+        gate.__generic_datatype_mismatch(
+            value_str=__account_str,
+            value_type=__account_type,
+            value_name="Account",
+            expected_name="AccountModel"
+        )
+
+    if __data_type is not dict:
+        gate.__generic_datatype_mismatch(
+            value_str=__data_str,
+            value_type=__data_type,
+            value_name="Data",
+            expected_name="dictionary"
+        )
+
+    __existing_data = get_data_by_account(
+        account=account
+    )
+
+    if __existing_data is not None:
+        return __existing_data
+    else:
+        __new_data = DataModel.create(
+            account=account,
+            data="",
+            nonce=""
+        )
+
+        __new_data.save()
+
+        return __new_data
