c66fe13b8e60ba95f95b6054b45f27758065c18b
pub-fn <globally.scoped@yahoo.com>
2022-12-29 00:48:57 -0500

    Working on interface functions, silences any exceptions/errors within the runtime code. Will be returned as a tuple, 1: result data, 2: exception.
    No result data = exception
    Result data = no exception

--- main.py
+++ main.py
@@ -1,20 +1,2 @@
 import scilla_db
 
-new_account = scilla_db.database.get_account(
-    username="username",
-)
-
-current_data = scilla_db.database.encrypt_and_overwrite_account_data(
-    account=new_account,
-    password="password",
-    data={
-        "ha": "ha1!"
-    }
-)
-
-returned_data = scilla_db.database.get_decrypted_account_data(
-    account=new_account,
-    password="password"
-)
-
-print(returned_data.get("ha"))
--- scilla_db/__init__.py
+++ scilla_db/__init__.py
@@ -1,2 +1,33 @@
 from scilla_db import database
 from scilla_db import gate
+
+
+def create_account(
+    username: str = "",
+    password: str = ""
+) -> (database.AccountModel, Exception):
+    __result, __exception = None, None
+    try:
+        __result = database.create_account(
+            username=username,
+            password=password
+        )
+    except Exception as e:
+        __exception = e
+    finally:
+        return __result, __exception
+
+
+def get_account(
+    username: str = ""
+) -> (database.AccountModel, Exception):
+    __result, __exception = None, None
+    try:
+        __result = database.get_account(
+            username=username
+        )
+    except Exception as e:
+        __exception = e
+    finally:
+        return __result, __exception
+
